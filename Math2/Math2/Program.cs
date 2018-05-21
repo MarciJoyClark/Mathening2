85b49ef5b20e120163c94435d0df3f3068b9c1bc
MarciJoyClark <33448580+MarciJoyClark@users.noreply.github.com>
2018-05-20 23:56:38 -0700

    Math2
    Upload

---
+++ Math2/Math2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MathOp mathObject = new MathOp();
+            Console.WriteLine("501 divided by 2 gives us a remainder of: " + mathObject.MathOverload(501));
+            Console.WriteLine("7.1365 * 7, then rounded to a whole number gives us " + mathObject.MathOverload(7.1365m));
+            Console.WriteLine("A string object of 10 converted to Int32 + 5 is " + mathObject.MathOverload("5") + ". If I gave it something that it couldn't convert to an int, the program would auto-insert 5.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathOp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math2
+{
+    public class MathOp
+    {
+        public int MathOverload(int number)
+        {
+            int newNumber = number % 2;
+            return newNumber;
+        }
+        public int MathOverload(decimal number)
+        {
+            int newNumber = Convert.ToInt32(number * 7);
+            return newNumber;
+        }
+        public int MathOverload(string number)
+        {
+            try
+            {
+                int newNumber = Convert.ToInt32(number) + 10;
+                return newNumber;
+            }
+            catch (FormatException)
+            {
+                return 5;
+            }
+        }
+    }
+}
